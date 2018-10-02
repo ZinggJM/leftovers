daa351466af942ad71e716317af02f7b20f1ad23
ZinggJM <jean-marc.zingg@bluewin.ch>
2018-10-02 08:57:41 +0200

    add example Umlauts

---
+++ examples/Umlauts/Umlauts.ino
@@ -0,0 +1,48 @@
+// Umlauts : check Umlauts encoding
+
+#include <Adafruit_GFX.h>
+
+const char umlauts[] = "ÄÖÜäéöü";
+
+const int base = HEX;
+
+class CheckPrintClass : public Print
+{
+    size_t write(uint8_t v)
+    {
+      Serial.print(v, base); Serial.print(" ");
+    }
+} CheckPrint;
+
+class CheckGfxClass : public Adafruit_GFX
+{
+  public:
+    CheckGfxClass() : Adafruit_GFX(100, 100) {};
+    void drawPixel(int16_t x, int16_t y, uint16_t color) {};
+    size_t write(uint8_t v)
+    {
+      Serial.print(v, base); Serial.print(" ");
+    }
+} CheckGfx;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Hello Umlauts!");
+  Serial.print("sizeof(umlauts) is "); Serial.println(sizeof(umlauts));
+  for (int i = 0; i < sizeof(umlauts) - 1; i++)
+  {
+    Serial.write(umlauts[i]); Serial.print(" : ");
+    Serial.print(uint16_t(umlauts[i])); Serial.print(" ");
+    CheckPrint.print(umlauts[i]);
+    CheckGfx.print(umlauts[i]);
+    Serial.println();
+  }
+  CheckPrint.print(umlauts); Serial.println();
+  CheckGfx.print(umlauts); Serial.println();
+}
+
+void loop()
+{
+}
---
+++ src/leftovers.h
@@ -0,0 +1,3 @@
+// leftovers.h
+
+// used to make it look like an Arduino library
